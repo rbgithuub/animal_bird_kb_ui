d77af4ed52c90685d6cbab7adb3cd5cad32e6171
Balaji Raja <net.balajiraja@gmail.com>
2025-04-17 03:31:21 +0530

    Made changes to CRUD operation towards swagger API interface in app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from pymongo import MongoClient
 from bson import ObjectId
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
 CORS(app)
@@ -10,11 +11,27 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["animalDB"]
 collection = db["animals"]
 
+# Initialize Swagger
+swagger = Swagger(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/animals', methods=['GET'])
+@swag_from({
+    'tags': ['Animals'],
+    'responses': {
+        200: {
+            'description': 'List of animals',
+            'schema': {
+                'type': 'array',
+                'items': {'type': 'object'}
+            }
+        }
+    }
+})
+
 def get_animals():
     animals = list(collection.find())
     for animal in animals:
@@ -22,18 +39,77 @@ def get_animals():
     return jsonify(animals)
 
 @app.route('/animals', methods=['POST'])
+@swag_from({
+    'tags': ['Animals'],
+    'parameters': [{
+        'name': 'body',
+        'in': 'body',
+        'required': True,
+        'schema': {
+            'type': 'object',
+            'properties': {
+                'name': {'type': 'string'},
+                'category': {'type': 'string'},
+                'origin': {'type': 'string'},
+                'sleep_pattern': {'type': 'string'},
+                'food_habits': {'type': 'string'},
+                'fun_facts': {
+                    'type': 'object',
+                    'properties': {
+                        '1': {'type': 'string'},
+                        '2': {'type': 'string'}
+                    }
+                }
+            }
+        }
+    }],
+    'responses': {
+        200: {'description': 'Animal added successfully'}
+    }
+})
 def add_animal():
     data = request.json
     result = collection.insert_one(data)
     return jsonify({'inserted_id': str(result.inserted_id)})
 
 @app.route('/animals/<id>', methods=['PUT'])
+@swag_from({
+    'tags': ['Animals'],
+    'parameters': [
+        {'name': 'id', 'in': 'path', 'type': 'string', 'required': True},
+        {'name': 'body', 'in': 'body', 'required': True, 'schema': {
+            'type': 'object',
+            'properties': {
+                'name': {'type': 'string'},
+                'category': {'type': 'string'},
+                'origin': {'type': 'string'},
+                'sleep_pattern': {'type': 'string'},
+                'food_habits': {'type': 'string'},
+                'fun_facts': {
+                    'type': 'object',
+                    'properties': {
+                        '1': {'type': 'string'},
+                        '2': {'type': 'string'}
+                    }
+                }
+            }
+        }}
+    ],
+    'responses': {200: {'description': 'Animal updated successfully'}}
+})
+
 def update_animal(id):
     data = request.json
     collection.update_one({'_id': ObjectId(id)}, {'$set': data})
     return jsonify({'message': 'Animal updated'})
 
 @app.route('/animals/<id>', methods=['DELETE'])
+@swag_from({
+    'tags': ['Animals'],
+    'parameters': [{'name': 'id', 'in': 'path', 'type': 'string', 'required': True}],
+    'responses': {200: {'description': 'Animal deleted successfully'}}
+})
+
 def delete_animal(id):
     collection.delete_one({'_id': ObjectId(id)})
     return jsonify({'message': 'Animal deleted'})
