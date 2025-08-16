4aafddc2b15524e297eb4503321c495c2edb9a2a
Balaji Raja <net.balajiraja@gmail.com>
2025-08-17 01:10:51 +0530

    Working version of docker compose - animal kb app

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def delete_animal(id):
     return jsonify({'message': 'Animal deleted'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
